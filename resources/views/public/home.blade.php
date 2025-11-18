86f59292dc49acc8cad619963ff39ab0abbec84a
JDavid Lugo <jodaluca@hotmail.com>
2025-11-18 18:04:54 -0400

    changed footer

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -207,9 +207,7 @@
             <!-- footer start -->
             <footer class="overflow-hidden rounded-b-2xl" style="background: transparent">
                 <p class="text-center py-6 text-gray-lite dark:text-color-910"> © 2023 Todos los derechos reservados
-                    by <a class="hover:text-[#FA5252] duration-300 transition"
-                        href="me.elrincondeisma.com" target="_blank"
-                        rel="noopener noreferrer"> JDavid-lugo</a>. </p>
+                    by <a class="hover:text-[#FA5252] duration-300 transition" href="" target="_blank" rel="noopener noreferrer"> Osadia.de</a>. </p>
             </footer>
             <!-- footer section end -->
         </div>
